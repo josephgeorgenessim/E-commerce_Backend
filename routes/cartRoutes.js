5467839657f911257c39b42959123ffc32fc15fe
JosephNessim <josephgeorgenessim@gmail.com>
2025-09-03 23:12:09 +0300

    apply Coupon To Cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { protect, allowedTo } = require('../services/authServices');
-const { addProductToCart, removeProductFromCart, getUserCart, updateProductQuantityInCart, clearCart } = require('../services/cartServices');
+const { addProductToCart, removeProductFromCart, getUserCart, updateProductQuantityInCart, clearCart, applyCouponToCart } = require('../services/cartServices');
 
 const router = express.Router();
 router.route('/clear').delete(protect, allowedTo('user'), clearCart)
+router.route('/coupon').post(protect, allowedTo('user'), applyCouponToCart)
 
 
 router.route('/').get(protect, allowedTo('user'), getUserCart).post(protect, allowedTo('user'), addProductToCart)
--- services/cartServices.js
+++ services/cartServices.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const ApiError = require('../utils/apiError');
 const Cart = require('../models/cartModel');
+const Coupon = require('../models/couponModel');
 const Products = require('../models/productModel');
 
 
@@ -82,7 +83,7 @@ exports.addProductToCart = asyncHandler(async (req, res, next) => {
     // calculate total cart price
     const totalCartPrice = calculateTotalCartPrice(userCart)
     userCart.totalCartPrice = totalCartPrice
-
+    userCart.totalPriceAfterDiscount = null
     await userCart.save()
     return res.status(200).json({
         status: 'success',
@@ -125,13 +126,13 @@ exports.updateProductQuantityInCart = asyncHandler(async (req, res, next) => {
     const userCart = await Cart.findOne({ user: req.user._id });
     const productInCart = userCart.CartItems.findIndex(item => item._id.toString() === itemId)
 
-    if(productInCart > -1){
+    if (productInCart > -1) {
         const cartItem = userCart.CartItems[productInCart]
         cartItem.quantity = quantity
         userCart.CartItems[productInCart] = cartItem
-        console.log("here in index",cartItem)
-        console.log("here in userCart",userCart)
-    }else{
+        console.log("here in index", cartItem)
+        console.log("here in userCart", userCart)
+    } else {
         return next(new ApiError('Product not found in cart', 404))
     }
     if (!userCart) {
@@ -159,5 +160,39 @@ exports.clearCart = asyncHandler(async (req, res, next) => {
     return res.status(200).json({
         status: 'success',
         message: 'Cart cleared successfully'
+    })
+})
+
+
+// @desc    Apply coupon to user cart
+// @route   POST /api/v1/cart/coupon
+// @access  protected / user
+exports.applyCouponToCart = asyncHandler(async (req, res, next) => {
+    const { coupon } = req.body;
+    const userCart = await Cart.findOne({ user: req.user._id });
+    const couponIsHere = await Coupon.findOne({ name: coupon, expiryDate: { $gte: new Date() } });
+
+    if (!couponIsHere) {
+        return next(new ApiError('Coupon is invalid or expired', 404))
+    }
+
+    if (!userCart) {
+        return next(new ApiError('Cart not found', 404))
+    }
+    console.log(userCart)
+    const totalCartPrice = userCart.totalCartPrice
+    console.log(totalCartPrice)
+    console.log(couponIsHere.discount)
+    const totalPriceAfterDiscount = (totalCartPrice - (totalCartPrice * couponIsHere.discount) / 100).toFixed(2)
+    console.log(totalPriceAfterDiscount)
+    userCart.totalPriceAfterDiscount = totalPriceAfterDiscount
+
+    await userCart.save()
+    return res.status(200).json({
+        status: 'success',
+        message: 'Coupon applied successfully',
+        discount: couponIsHere.discount,
+        totalCartItems: userCart.CartItems.length,
+        cart: userCart
     })
 })
